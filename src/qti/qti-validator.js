23ae3f61781f6dc4c0b29b79195efbb64980dac3
Andriyan Ivanov <clickbuster1@gmail.com>
2017-04-07 13:07:43 +0700

    ADDED QTIValidator unified formatting for string value comparison

--- src/qti/qti-validator.js
+++ src/qti/qti-validator.js
@@ -1,7 +1,20 @@
 import QTIParser from './qti-parser';
 import * as QTIElements from './qti-elements';
 
+
 class QTIValidator {
+  constructor() {
+    this.decimalSeparator = '.'; // default decimal separator
+    
+    // @TODO remove and set per publisher
+    this.decimalSeparator = ',';
+  }
+  
+  setDecimalSeparator(separator) {
+    this.decimalSeparator = separator;
+    return this;
+  }
+  
   extractUserAnswer(inputNode) {
     const questionType = inputNode.getAttribute('question-type');
     
@@ -68,10 +81,31 @@ class QTIValidator {
       }
     }
     else {
-      result = String(answer).trim() === String(userAnswer).trim();
+      result = this.uniformatValue(answer) === this.uniformatValue(userAnswer);
     }
     
     return result;
+  }
+  
+  uniformatValue(value) {
+    value = String(value);              // stringify
+    value = value.toLowerCase();        // cast to lowercase
+    value = value.replace(/ /g, '');    // remove spaces
+    
+    // replace decimal separator
+    if(this.decimalSeparator !== '.') {
+      const decimalRegex = new RegExp(`${this.decimalSeparator}`, 'g');
+      value = value.replace(decimalRegex, '.');
+    }
+    
+    // remove preceeding zeros
+    value = value.replace(/^0+/, '');
+    // remove trailing zeros after last decimal separator
+    value = value.replace(/\.(?=[^.]*$)([^0]*)0+$/, '.$1');
+    // remove trailing decimal separators
+    value = value.replace(/\.+$/, '');
+    
+    return value;
   }
 }
 
