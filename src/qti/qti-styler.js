94569868cccc4864c6e3d2071aca0c32dc8cb076
Andriyan Ivanov <clickbuster1@gmail.com>
2017-05-02 19:12:05 +0300

    ADDED validation styling for SELECT fields

--- src/qti/qti-styler.js
+++ src/qti/qti-styler.js
@@ -4,6 +4,9 @@ import * as QTIElements from './qti-elements';
 const CORRECT = 'hide-correct';
 const INCORRECT = 'hide-incorrect';
 
+const CORRECT_OPTION = 'hide-correct-value';
+const INCORRECT_OPTION = 'hide-incorrect-value';
+
 class QTIStyler {
   resetStyles(node) {
     node.classList.remove(CORRECT, INCORRECT);
@@ -26,7 +29,7 @@ class QTIStyler {
     let node, isValid, questionType;
     
     for(let i = 0; i < answerArray.length; i++) {
-      { node } = userAnswers[i];
+      node = userAnswers[i].node;
       isValid = answerArray[i];
       questionType = node.getAttribute('question-type');
       
@@ -37,6 +40,7 @@ class QTIStyler {
           break;
           
         case QTIElements.inlineChoiceInteraction.IDENTIFIER:
+          return this.validateSelectInput(node, isValid, hasUsedLastChance);
           break;
           
         case QTIElements.choiceInteraction.IDENTIFIER:
@@ -63,6 +67,36 @@ class QTIStyler {
     
     // set correctness class in all cases
     return this.setCorrectnessClass(node, isValid);
+  }
+  
+  validateSelectInput(node, isValid, hasUsedLastChance) {
+    const select = node.getElementsByTagName('select')[0];
+    let option;
+    
+    if(!select) {
+      throw 'Select field was not found in the provided inputNode';
+    }
+    
+    // default option selected
+    if(select.selectedIndex === 0) {
+      return;
+    }
+    
+    for(let i = 0; i < select.options.length; i++) {
+      option = select.options[i];
+      
+      // disable all checkboxes if is valid or was last chance
+      if(hasUsedLastChance || isValid) {
+        this.disable(option);
+      }
+      
+      // add correctness class to the selected index
+      if(i === select.selectedIndex) {
+        option.classList.add(isValid ? CORRECT_OPTION : INCORRECT_OPTION);
+      }
+    }
+    
+    return this.setCorrectnessClass(select, isValid);
   }
 }
 
