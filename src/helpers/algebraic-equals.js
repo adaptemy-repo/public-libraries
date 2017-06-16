410828c37399c1bdca9c7e51f80c44203578f083
Andriyan Ivanov <clickbuster1@gmail.com>
2017-06-16 23:26:19 +0300

    RENAME compare() helper now algebraicEquals()

--- src/helpers/algebraic-equals.js
+++ src/helpers/algebraic-equals.js
@@ -1,6 +1,6 @@
 'use strict';
 
-export default function compare(a,b){
+export default function algebraicEquals(a,b){
   var dimension = getDimension(a);
   var errorFound = false;
   dimension = dimension || 1; //set minimum value to 1, we want to test problems of 0 dimension
