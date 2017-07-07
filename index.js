35e9bef56f3c403dc0267aec72d2f04121feea9e
Andriyan Ivanov <clickbuster1@gmail.com>
2017-07-07 03:41:35 +0300

    ADDED webpack config for running tests

---
+++ index.js
@@ -0,0 +1,7 @@
+export { default as replaceVariables } from './src/qti/helpers/replaceVariables';
+export { default as variabilizeNode } from './src/qti/helpers/variabilizeNode';
+
+export { default as QTIGenerator } from './src/qti/qti-generator';
+export { default as QTIStyler } from './src/qti/qti-styler';
+export { default as QTIParser } from './src/qti/qti-parser';
+export { default as QTIValidator } from './src/qti/qti-validator';
