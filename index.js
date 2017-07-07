d6c7627bfb060d865923602b30b193f7093bded6
Andriyan Ivanov <clickbuster1@gmail.com>
2017-07-07 03:49:27 +0300

    UPDATED index.js (bundle) with helpers index

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+export * from './src/helpers';
 export { default as replaceVariables } from './src/qti/helpers/replaceVariables';
 export { default as variabilizeNode } from './src/qti/helpers/variabilizeNode';
 
