bbab715ed8c28da78afe2259cbedb1ffd1b1f1de
Andriyan Ivanov <clickbuster1@gmail.com>
2017-07-07 03:51:29 +0300

    BUGFIX path typo

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,3 @@
-export { default as algebraicEquals } from './algebratic-equals';
+export { default as algebraicEquals } from './algebraic-equals';
 export * from './extract-html';
 export * from './pseudo-rng';
